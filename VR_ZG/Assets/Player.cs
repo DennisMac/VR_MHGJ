19886cb74b155a68ca6d8af9cf970ad2b9bfa5d2
DennisMac <d12768@gmail.com>
2018-04-13 16:16:43 -0400

    one

---
+++ VR_ZG/Assets/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        this.transform.position += new Vector3(h,0,v);
+	}
+}
